468e1f6c9e718e14b93ef47c8c959b350ef405d0
Alejandro Saldarriaga <alejandroasp@gmail.com>
2019-02-19 11:48:07 -0500

    feature - include hot reloader and initial design

---
+++ src/components/header/header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+// @assets
+import logo from './logo.svg';
+// @material
+import Button from '@material-ui/core/Button';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="app__header">
+        <img src={logo} className="app__logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="app__link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button variant="outlined" color="primary">
+            Learn React
+          </Button>
+        </a>
+      </header>
+    );
+  }
+}
+
+export default Header;
---
+++ src/routes/app/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader/root';
+import './App.scss';
+// @components
+// import Header from './../../components/header/header';
+import Catalog from './../../components/catalog/catalog';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Catalog />
+      </div>
+    );
+  }
+}
+
+export default hot(App);
