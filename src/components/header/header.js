90cecb75edcb740e27de90bf32a4133a206ed9df
Alejandro Saldarriaga <alejandroasp@gmail.com>
2019-02-20 10:37:08 -0500

    feature - code beautifier

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,26 +5,26 @@ import logo from './logo.svg';
 import Button from '@material-ui/core/Button';
 
 class Header extends Component {
-  render() {
-    return (
-      <header className="app__header">
-        <img src={logo} className="app__logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="app__link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button variant="outlined" color="primary">
-            Learn React
-          </Button>
-        </a>
-      </header>
-    );
-  }
+    render() {
+        return (
+            <header className="app__header">
+                <img src={logo} className="app__logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="app__link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Button variant="outlined" color="primary">
+                        Learn React
+                    </Button>
+                </a>
+            </header>
+        );
+    }
 }
 
 export default Header;
