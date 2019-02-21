2fb499ba46f7bb9686a24f0e3d927688cb801a67
Alejandro Saldarriaga <alejandroasp@gmail.com>
2019-02-21 09:59:55 -0500

    feature - lazy load and infinite scroller

--- src/components/catalog/catalog.js
+++ src/components/catalog/catalog.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import get from 'lodash/get';
+import LazyLoad from 'react-lazyload';
+import InfiniteScroll from 'react-infinite-scroller';
 // @material
 import {
     CssBaseline,
@@ -20,9 +22,43 @@ import { backup } from './../../data/db.json';
 import catalogStyles from './catalogStyles';
 
 class Catalog extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            games: get(backup, 'Game', []).slice(0, 20),
+            hasMoreItems: true
+        };
+
+        this.loadItems = this.loadItems.bind(this);
+    }
+
+    loadItems(page) {
+        const load = 20;
+        const start = page * load;
+        const items = get(backup, 'Game', []);
+        const games = items.slice(0, start + load);
+        const hasMoreItems = items.length >= start;
+
+        this.setState({ games, hasMoreItems });
+    }
+
     render() {
         const { classes } = this.props;
-        const games = get(backup, 'Game', []).slice(0, 10);
+        const { games, hasMoreItems } = this.state;
+
+        const loader = <div className="loader">Loading ...</div>;
+
+        const items = games.map(card => (
+            <Grid item key={get(card, 'id')} sm={6} md={4} lg={3}>
+                <LazyLoad height={250}>
+                    <GameCard
+                        title={get(card, 'name', '')}
+                        image={get(card, 'image_url_medium', '')}
+                    />
+                </LazyLoad>
+            </Grid>
+        ));
 
         return (
             <React.Fragment>
@@ -30,21 +66,21 @@ class Catalog extends Component {
                 <Navigator />
                 <main>
                     <Title />
-                    <div className={classNames(classes.layout, classes.cardGrid)}>
-                        <Grid container spacing={40}>
-                            {games.map(card => (
-                                <Grid item key={get(card, 'id')} sm={6} md={4} lg={3}>
-                                    <GameCard
-                                        title={get(card, 'name', '')}
-                                        image={get(card, 'image_url_medium', '')}
-                                    />
-                                </Grid>
-                            ))}
-                        </Grid>
-                    </div>
+                    <InfiniteScroll
+                        pageStart={0}
+                        loadMore={this.loadItems}
+                        hasMore={hasMoreItems}
+                        loader={loader}
+                    >
+                        <div className={classNames(classes.layout, classes.cardGrid)}>
+                            <Grid container spacing={40}>
+                                {items}
+                            </Grid>
+                        </div>
+                    </InfiniteScroll>
                 </main>
                 <Footer />
-            </React.Fragment>
+            </React.Fragment >
         );
     }
 }
