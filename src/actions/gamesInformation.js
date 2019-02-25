2a9a308dc767c7233df89e3ed0da39830e2e300c
Wilfer Arley Salazar Giraldo <wilfersalazar19@gmail.com>
2019-02-25 15:55:16 -0500

    fix #15 - Add Labels to catalog and color the labels

--- src/actions/gamesInformation.js
+++ src/actions/gamesInformation.js
@@ -1,8 +1,11 @@
 //@vendor
 import { createActions } from 'redux-actions';
 import get from 'lodash/get';
+import filter from 'lodash/filter';
+import find from 'lodash/find';
 // @constants
 import { backup } from './../data/db.json';
+import { ITEMS_BY_PAGE } from './../constants/index';
 
 const actions = createActions(
     'LOAD_JSON_INFORMATION',
@@ -14,12 +17,22 @@ const actions = createActions(
 
 const initGames = () => (dispatch) => {
     const games = get(backup, 'Game', []).slice(0);
-    dispatch(actions.loadJsonInformation({ response: { games } }));
+    const gamesLabels = get(backup, 'GameLabel', []);
+    const labels = get(backup, 'Label', []);
+    const platforms = get(backup, 'Platform', []);
+
+    const gamesWithLabels = games.map(game => {
+        const gameLabels = filter(gamesLabels, gameLabel => gameLabel.game_id === game.id);
+        game.labels = gameLabels.map(gameLabel => find(labels, label => label.id === gameLabel.label_id));
+        return game;
+    });
+
+    dispatch(actions.loadJsonInformation({ response: { games: gamesWithLabels, platforms } }));
 };
 
 const loadGames = (page = 0) => (dispatch, getState) => {
     const gamesInformation = getState().gamesInformation;
-    const load = 20;
+    const load = ITEMS_BY_PAGE;
     const start = page * load;
     const games = gamesInformation.get('source').slice(0, start + load);
     const hasMoreItems = games.length >= length;
