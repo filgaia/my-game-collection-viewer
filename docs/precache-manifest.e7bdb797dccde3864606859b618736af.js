163e3afb87b2de3b45e5ac3efcb52ed570251d35
Alejandro Saldarriaga <alejandroasp@gmail.com>
2019-09-04 11:38:50 -0500

    feature - move build to docs folder

---
+++ docs/precache-manifest.e7bdb797dccde3864606859b618736af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35d853931c851106b5f6",
+    "url": "/my-game-collection-viewer/static/js/runtime~main.35d85393.js"
+  },
+  {
+    "revision": "6d7df1960fa78c218002",
+    "url": "/my-game-collection-viewer/static/js/main.6d7df196.chunk.js"
+  },
+  {
+    "revision": "07cdfd405071f2b815fe",
+    "url": "/my-game-collection-viewer/static/js/1.07cdfd40.chunk.js"
+  },
+  {
+    "revision": "6d7df1960fa78c218002",
+    "url": "/my-game-collection-viewer/static/css/main.8b3247db.chunk.css"
+  },
+  {
+    "revision": "d1f6cff6756483c9a825d22a3165757e",
+    "url": "/my-game-collection-viewer/index.html"
+  }
+];
